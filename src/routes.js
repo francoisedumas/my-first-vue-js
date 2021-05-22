4890151ea1c845f96fff9ee479b311ce48318bab
Francois Dumas <dumaslattaque.francois@gmail.com>
2021-05-22 23:13:29 +0200

    individual blog post

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import showBlogs from './components/showBlogs.vue'
 import addBlog from './components/addBlog.vue'
 import listBlogs from './components/listBlogs.vue'
+import singleBlog from './components/singleBlog.vue'
 
 export default [
     { path: '/show', component: showBlogs},
     { path: '/list', component: listBlogs},
-    { path: '/add', component: addBlog}
+    { path: '/add', component: addBlog},
+    { path: '/blog/:id', component: singleBlog}
 ]
